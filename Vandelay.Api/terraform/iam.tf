8eb71b586b4ef0fec1bd4d759f1b046fe47fef0a
augustandresmeyer <augustmeyermakesgames@gmail.com>
2025-08-08 14:16:32 -0500

    update iam.tf to allow Lambda to GetSecretValue

--- Vandelay.Api/terraform/iam.tf
+++ Vandelay.Api/terraform/iam.tf
@@ -17,3 +17,20 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
     role = aws_iam_role.lambda_exec.name
     policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+data "aws_iam_policy_document" "lambda_read_db_secret" {
+    statement {
+        actions = ["secretsmanager:GetSecretValue"]
+        resources = [aws_secretsmanager_secret.db_credentials.arn]
+    }
+}
+
+resource "aws_iam_policy" "lambda_read_db_secret" {
+    name = "vandelay-lambda-read-db-secret"
+    policy = data.aws_iam_policy_document.lambda_read_db_secret.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_read_db_secret" {
+    role = aws_iam_role.lambda_exec.name
+    policy_arn = aws_iam_policy.lambda_read_db_secret.arn
+}
