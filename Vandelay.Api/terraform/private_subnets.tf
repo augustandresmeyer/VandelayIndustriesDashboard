e2d46f4e9dbae039d801b68ce3fde2e6b50de4a6
augustandresmeyer <augustmeyermakesgames@gmail.com>
2025-08-10 00:45:25 -0500

    add private_subnets.tf and terraform.tfvars

---
+++ Vandelay.Api/terraform/private_subnets.tf
@@ -0,0 +1,31 @@
+resource "aws_subnet" "private_1" {
+    vpc_id = var.vpc_id
+    cidr_block = var.private_subnet_cidr1
+    availability_zone = var.private_subnet_az1
+    map_public_ip_on_launch = false
+    tags = { Name = "vandelay-private-1" }
+}
+
+resource "aws_subnet" "private_2" {
+    vpc_id = var.vpc_id
+    cidr_block = var.private_subnet_cidr2
+    availability_zone = var.private_subnet_az2
+    map_public_ip_on_launch = false
+    tags = { Name = "vandelay-private-2" }
+}
+
+resource "aws_route_table" "private_rt" {
+    vpc_id = var.vpc_id
+    tags = { Name = "vandelay-private-rt" }
+}
+
+resource "aws_route_table_association" "assoc_1" {
+    subnet_id = aws_subnet.private_1.id
+    route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "assoc_2" {
+    subnet_id = aws_subnet.private_2.id
+    route_table_id = aws_route_table.private_rt.id
+}
+
