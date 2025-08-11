07d497af8415131fb9e5337fde88c214bab9ff84
augustandresmeyer <augustmeyermakesgames@gmail.com>
2025-08-11 18:21:16 -0500

    add rds sg to terraform

---
+++ Vandelay.Api/terraform/rds.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "rds_sg" {
+    name_prefix = "vandelay-rds-"
+    description = "Security group for RDS PostgreSQL"
+    vpc_id = var.vpc_id
+
+    ingress {
+        from_port = var.db_port
+        to_port = var.db_port
+        protocol = "tcp"
+        security_groups = [aws_security_group.lambda_sg.id]
+        description = "PostgreSQL from Lambda"
+    }
+
+    tags = {
+        Name = "vandelay-rds-sg"
+    }
+}
+
