e15fe9f0b133a79448fd05a6a806b0795ba64cfa
augustandresmeyer <augustmeyermakesgames@gmail.com>
2025-11-01 19:47:41 -0500

    updating outputs for lambda function name and rds endpoint

--- Vandelay.Api/terraform/outputs.tf
+++ Vandelay.Api/terraform/outputs.tf
@@ -1,4 +1,16 @@
 output "api_endpoint" {
-    value = aws_apigatewayv2_api.http_api.api_endpoint
+    value       = aws_apigatewayv2_api.http_api.api_endpoint
+    description = "API Gateway endpoint URL"
+}
+
+output "lambda_function_name" {
+    value       = aws_lambda_function.vandelay_api.function_name
+    description = "Lambda function name"
+}
+
+output "rds_endpoint" {
+    value       = aws_db_instance.vandelay_postgres.endpoint
+    description = "RDS PostgreSQL endpoint"
+    sensitive   = true
 }
 
